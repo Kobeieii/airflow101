79d1a44e7dc435c29f73c5084905b8b8c752e3d6
Kobeieii <ploy.thitiworada@outlook.com>
2024-12-20 10:21:17 +0700

    feat(dag): create django_model dag

--- dags/2_django_model.py
+++ dags/2_django_model.py
@@ -21,13 +21,50 @@ with DAG(
     catchup=False,
     tags=["app"],
 ) as dag:
+    import pandas as pd
+    import requests
+    from django_app.apps.models.report import Report
     
     def taskflow_api():
         
         @task
-        def test():
-            print(os.environ.get('DJANGO_SETTINGS_MODULE'))
+        def get_users():
+            response = requests.get("https://jsonplaceholder.typicode.com/users")
+            users = response.json()
+            return users
         
-        test()
+        @task
+        def get_posts():
+            response = requests.get("https://jsonplaceholder.typicode.com/posts")
+            posts = response.json()
+            return posts
+        
+        @task
+        def generate_report(users, posts):
+            users_df = pd.DataFrame(users)
+            posts_df = pd.DataFrame(posts)
+            report = users_df.merge(posts_df, left_on="id", right_on="userId")
+            return report
+        
+        @task
+        def save_report(report):
+            reports = [Report(
+                user_id=row["userId"],
+                name=row["name"],
+                username=row["username"],
+                email=row["email"],
+                address=row["address"],
+                phone=row["phone"],
+                website=row["website"],
+                company=row["company"],
+                title=row["title"],
+                body=row["body"],
+            ) for _, row in report.iterrows()]
+            Report.objects.bulk_create(reports)
+
+        users = get_users()
+        posts = get_posts()
+        report_df = generate_report(users, posts)
+        save_report(report_df)
         
     taskflow_api()
